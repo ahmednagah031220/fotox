a6cc7a1988202d32beccf06dca590a39ec21cd8f
Jana10101 <138763028+Jana10101@users.noreply.github.com>
2024-12-21 21:53:31 +0200

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -274,13 +274,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
   const firebaseConfig = {
-    apiKey: "AIzaSyCfUc3IqvU-GF_M8VPLhutF0K15WGVu35A",
-    authDomain: "fotox-c09f7.firebaseapp.com",
-    projectId: "fotox-c09f7",
-    storageBucket: "fotox-c09f7.firebasestorage.app",
-    messagingSenderId: "879587849698",
-    appId: "1:879587849698:web:57b2afdd196586785535bb"
-  };
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
 
