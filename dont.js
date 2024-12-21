ef32b19681ff6e1415bd550367347d99519c8681
Jana10101 <138763028+Jana10101@users.noreply.github.com>
2024-12-21 21:55:50 +0200

    Delete dont.js

--- dont.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCfUc3IqvU-GF_M8VPLhutF0K15WGVu35A",
-    authDomain: "fotox-c09f7.firebaseapp.com",
-    projectId: "fotox-c09f7",
-    storageBucket: "fotox-c09f7.firebasestorage.app",
-    messagingSenderId: "879587849698",
-    appId: "1:879587849698:web:57b2afdd196586785535bb"
-};
